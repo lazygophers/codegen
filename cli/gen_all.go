3405e137f1602c1ed9738db890b2787d89f7a366
落心 <33619903+Luoxin@users.noreply.github.com>
2024-03-04 18:35:51 +0800

    Generate all action

---
+++ cli/gen_all.go
@@ -0,0 +1,31 @@
+package cli
+
+import (
+	"github.com/lazygophers/log"
+	"github.com/spf13/cobra"
+)
+
+var genAllCmd = &cobra.Command{
+	Use:   "all",
+	Short: "gen all action",
+	Long:  `Generate all action.`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		err = runGenPb(cmd, args)
+		if err != nil {
+			log.Errorf("err:%v", err)
+			return err
+		}
+
+		err = runGenMod(cmd, args)
+		if err != nil {
+			log.Errorf("err:%v", err)
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	genCmd.AddCommand(genAllCmd)
+}
